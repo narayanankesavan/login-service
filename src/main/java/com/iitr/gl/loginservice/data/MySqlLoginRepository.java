29673296bfb9976b10c716c9a4acf36a71863aa3
narayanankesavan <87275171+narayanankesavan@users.noreply.github.com>
2022-09-02 20:10:49 +0530

    Delete src directory

--- src/main/java/com/iitr/gl/loginservice/data/MySqlLoginRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iitr.gl.loginservice.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MySqlLoginRepository extends JpaRepository<UserEntity, Integer> {
-    UserEntity findByEmail(String email);
-}
